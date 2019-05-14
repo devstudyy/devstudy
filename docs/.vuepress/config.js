9326c13da2c6fe3d26c27e799f71745ea2c8dafa
Yukyung <yukyung204@interpark.com>
2019-05-14 14:13:03 +0900

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    home: true,
+    title: 'Dev Study',
+    description: 'dev study blog',
+    base: '/devstudy/',
+    themeConfig: {
+        repo: 'devstudyy/devstudy',
+        editLinks: true,
+        docsDir: 'docs',
+        lastUpdated: 'Last Updated',
+        search: true,
+        sidebarDepth: 3,
+        nav: [{
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: '김유경',
+                link: '/yukyung/'
+            }
+        ],
+        sidebar: {
+            '/yukyung/': [{
+                title: 'YUKYUNG',
+                collapsable: true,
+                children: [
+                    'one'
+                ]
+            }]
+        }
+    },
+}
