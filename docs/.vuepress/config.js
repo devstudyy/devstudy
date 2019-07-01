874fe61b02f2a08576270c975261c96d6f6850cc
hynw <yyff2222@gmail.com>
2019-07-01 19:37:54 +0900

    second commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,122 +10,135 @@ module.exports = {
         lastUpdated: 'Last Updated',
         search: true,
         nav: [
-              {
-                  text: 'Home',
-                  link: '/'
-              },
-              {
-                  text: '김유경',
-                  link: '/yukyung/'
-              },
-              {
-                  text: '황주리',
-                  link: '/jju/'
-              },
-              {
-                  text: '마성민',
-                  link: '/ma/'
-              },
-              {
-                  text: '백승빈',
-                  link: '/been/'
-              },
-              {
-                  text: '최승원',
-                  link: '/swchoi/'
-              },
-              {
-                  text: '윤종원',
-                  link: '/jjong/'
-              },
-              {
+            {
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: '김유경',
+                link: '/yukyung/'
+            },
+            {
+                text: '황주리',
+                link: '/jju/'
+            },
+            {
+                text: '마성민',
+                link: '/ma/'
+            },
+            {
+                text: '백승빈',
+                link: '/been/'
+            },
+            {
+                text: '최승원',
+                link: '/swchoi/'
+            },
+            {
+                text: '윤종원',
+                link: '/jjong/'
+            },
+            {
                 text: '강은지',
-                items : [
-                          {
-                              text: 'JAVASCRIPT',
-                              link: '/oriri/javascript/'
-                          },
-                          {
-                            text: 'JAVA',
-                            link: '/oriri/java/'
-                          }
+                items: [
+                    {
+                        text: 'JAVASCRIPT',
+                        link: '/oriri/javascript/'
+                    },
+                    {
+                        text: 'JAVA',
+                        link: '/oriri/java/'
+                    }
                 ]
-              }
+            },
+            {
+                text: '박현우',
+                link: '/rnmkr/'
+            },
         ],
         sidebar: {
             '/yukyung/': [
-                          {
-                            title: 'DOCUMENT',
-                            collapsable: true,
-                            children: [
-                                'vuepress',
-                                'markdown'
-                            ]
-                          }
+                {
+                    title: 'DOCUMENT',
+                    collapsable: true,
+                    children: [
+                        'vuepress',
+                        'markdown'
+                    ]
+                }
             ],
             '/jju/': [
-                        {
-                          title: 'DOCUMENT',
-                          collapsable: true,
-                          children: [
-                              'python'
-                          ]
-                        }
+                {
+                    title: 'DOCUMENT',
+                    collapsable: true,
+                    children: [
+                        'python'
+                    ]
+                }
             ],
             '/ma/': [
-                      {
-                        title: 'DOCUMENT',
-                        collapsable: true,
-                        children: [
-                            'JS-Handlebars', 'JS-Template'
-                        ]
-                    }
+                {
+                    title: 'DOCUMENT',
+                    collapsable: true,
+                    children: [
+                        'JS-Handlebars', 'JS-Template'
+                    ]
+                }
             ],
             '/been/': [
-                        {
-                          title: 'DOCUMENT',
-                          collapsable: true,
-                          children: [
-                              'meteor'
-                          ]
-                      }
+                {
+                    title: 'DOCUMENT',
+                    collapsable: true,
+                    children: [
+                        'meteor'
+                    ]
+                }
             ],
             '/swchoi/': [
-                          {
-                            title: 'DOCUMENT',
-                            collapsable: true,
-                            children: [
-                                'SpringBatch'
-                            ]
-                        }
+                {
+                    title: 'DOCUMENT',
+                    collapsable: true,
+                    children: [
+                        'SpringBatch'
+                    ]
+                }
             ],
             '/jjong/': [
-                          {
-                            title: 'DOCUMENT',
-                            collapsable: true,
-                        }
-                      ],
-            '/oriri/javascript/': [
-                                    {
-                                        'title': 'DesignPattern', 
-                                        'children': [
-                                            '/oriri/javascript/designPattern/',
-                                            '/oriri/javascript/designPattern/module',
-                                            '/oriri/javascript/designPattern/singleton',
-                                            '/oriri/javascript/designPattern/observer'
-                                        ]
-                                    }
+                {
+                    title: 'DOCUMENT',
+                    collapsable: true,
+                }
             ],
-          '/oriri/java/': [
-                            {
-                                'title': 'DesignPattern', 
-                                'children': [
-                                    '/oriri/java/designPattern/',
-                                    '/oriri/java/designPattern/singleton',
-                                    '/oriri/java/designPattern/strategy'
-                                ]
-                            }
-          ]
+            '/oriri/javascript/': [
+                {
+                    'title': 'DesignPattern',
+                    'children': [
+                        '/oriri/javascript/designPattern/',
+                        '/oriri/javascript/designPattern/module',
+                        '/oriri/javascript/designPattern/singleton',
+                        '/oriri/javascript/designPattern/observer'
+                    ]
+                }
+            ],
+            '/oriri/java/': [
+                {
+                    'title': 'DesignPattern',
+                    'children': [
+                        '/oriri/java/designPattern/',
+                        '/oriri/java/designPattern/singleton',
+                        '/oriri/java/designPattern/strategy'
+                    ]
+                }
+            ],
+            '/rnmkr/': [
+                {
+                    title: 'DOCUMENT',
+                    collapsable: true,
+                    children: [
+                        'ElasticSearch'
+                    ]
+                }
+            ],
         }
     },
 }
